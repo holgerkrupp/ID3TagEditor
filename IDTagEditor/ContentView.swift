ef53e070a850b98e4673b0191d583a5c6cb2e039
Holger Krupp <email@holgerkrupp.de>
2026-05-23 21:26:51 +0200

    Initial Commit

---
+++ IDTagEditor/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  IDTagEditor
+//
+//  Created by Holger Krupp on 23.05.26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ IDTagEditor/IDTagEditorApp.swift
@@ -0,0 +1,17 @@
+//
+//  IDTagEditorApp.swift
+//  IDTagEditor
+//
+//  Created by Holger Krupp on 23.05.26.
+//
+
+import SwiftUI
+
+@main
+struct IDTagEditorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
